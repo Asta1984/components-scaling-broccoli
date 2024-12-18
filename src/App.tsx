d5d8b2e6564bb114f0a06488e7ee3dc791d19baa
Asta1984 <salilmandal908@gmail.com>
2024-12-18 23:34:20 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { CardWithForm } from './components/walletcard'
+import { BackgroundGradient } from './components/ui/background-gradient'
+function App() {
+
+
+  return (
+    <>
+    <BackgroundGradient>
+    <CardWithForm/>
+    </BackgroundGradient>
+    </>
+  )
+}
+
+export default App
