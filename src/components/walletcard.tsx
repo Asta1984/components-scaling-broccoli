921a20b418ca51f623e538fa6e962e1d4f49c724
Asta1984 <salilmandal908@gmail.com>
2024-12-19 17:53:34 +0530

    final

--- src/components/walletcard.tsx
+++ src/components/walletcard.tsx
@@ -3,56 +3,50 @@ import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "../components/ui/card"
 import { HoverBorderGradient } from "./ui/hover-border-gradient"
 import { Input } from "../components/ui/input"
 import { Label } from "../components/ui/label"
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "../components/ui/select"
+import { Progress } from "../components/ui/progress"
+
+
 
 export function CardWithForm() {
   return (
-    <Card className="w-[350px] rounded-3xl bg-black/10">
+    <Card className="w-[350px] rounded-3xl bg-zinc-800">
       <CardHeader>
-        <CardTitle>Create project</CardTitle>
-        <CardDescription>Deploy your new project in one-click.</CardDescription>
+        <CardTitle className="text-2xl font-OnlinePrivileges bg-clip-text text-transparent bg-gradient-to-r from-yellow-400 to-teal-600 ">Zacro Presale Stage 1</CardTitle>
+        <CardDescription className="text font-Enterpriser bg-clip-text text-transparent bg-gradient-to-r from-teal-400 to-red-600">1 RXS = $0.150 <br />Next Stage Price = $ 0.175</CardDescription>
+        <div>
+          <Progress value={78} />
+        </div>
+        <CardDescription className='text-l font-OnlinePrivileges bg-clip-text text-transparent bg-gradient-to-r from-yellow-400 to-teal-600'>USD Raised: $31,082,607 / $33,125,000 <br />Tokens Sold: 366,381,767 / 380,000,000</CardDescription>
       </CardHeader>
       <CardContent>
         <form>
-          <div className="grid w-full items-center gap-4">
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">Name</Label>
-              <Input id="name" placeholder="Name of your project" />
+          <div className="grid w-full items-center gap-4 ">
+            <div className="flex flex-col-3 gap-6 mb-6">
+            <Button variant={"outline"} className="text font-Enterpriser bg-clip-text text-transparent bg-gradient-to-r from-red-600 to-blue-900">ETH</Button>
+            <Button variant={"outline"} className="text font-Enterpriser bg-clip-text text-transparent bg-gradient-to-r from-red-600 to-green-600">USDT</Button>
+            <Button variant={"outline"} className="text font-Enterpriser bg-clip-text text-transparent bg-gradient-to-r from-red-600 to-yellow-600">Card</Button>
+            </div>
+       
+            <div className="flex flex-col space-y-3 "> 
+              <Label htmlFor="name" className="text font-Enterpriser bg-clip-text text-transparent bg-gradient-to-r from-white to-red-600" >Amount in ETH You Pay</Label>
+              <Input id="name" placeholder="Enter Amount"/>
+              <Label htmlFor="framework" className="text-s font-Enterpriser bg-clip-text text-transparent bg-gradient-to-r from-white to-red-600">Amount in RXS You Receive</Label>
+              <Input id="name" placeholder="0" />
+              <div className="mx-20 w-full p-1">
+              <HoverBorderGradient>Connect Wallet</HoverBorderGradient>
+              </div>
             </div>
             <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="framework">Framework</Label>
-              <Select>
-                <SelectTrigger id="framework">
-                  <SelectValue placeholder="Select" />
-                </SelectTrigger>
-                <SelectContent position="popper">
-                  <SelectItem value="next">Next.js</SelectItem>
-                  <SelectItem value="sveltekit">SvelteKit</SelectItem>
-                  <SelectItem value="astro">Astro</SelectItem>
-                  <SelectItem value="nuxt">Nuxt.js</SelectItem>
-                </SelectContent>
-              </Select>
             </div>
           </div>
         </form>
       </CardContent>
-      <CardFooter className="flex justify-between">
-        <Button variant="outline">Cancel</Button>
-        <HoverBorderGradient>good</HoverBorderGradient>
-      </CardFooter>
     </Card>
   )
 }
