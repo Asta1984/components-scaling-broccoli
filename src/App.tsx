1071d127387ac6f05eeba911661404d9cff811f0
Asta1984 <salilmandal908@gmail.com>
2025-01-14 22:14:25 +0530

    backgroundtrail

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import './App.css'
 import SquaresDemo from './components/SquareBackground'
-import Mazerunner from './components/WarpDemo'
 function App() {
 
 
   return (
        <>
        <SquaresDemo />
-       <Mazerunner />
+
        </>
   )
 }
--- src/components/SquareBackground.tsx
+++ src/components/SquareBackground.tsx
@@ -1,8 +1,18 @@
-import { Squares } from "../components/ui/squares-background"
+import { Squares } from "../components/ui/squares-background";
+import { BorderTrail } from "./ui/border-trail";
+
 
 export default function SquaresDemo() {
   return (
-    <div className="relative mb-20 md:mb-40">
+    <>
+      <div className=" flex md:items-center justify-center ">
+    <img 
+        src= 'animatedmaci.gif' 
+        alt="Remote GIF"
+        className="h-28 w-28 md:h-56 md:w-56 mb-10"
+    />
+  </div>
+    <div className="relative mb-20 md:mb-40  rounded-3xl">
       <div className="h-screen">
         <Squares 
           direction="diagonal"
@@ -12,13 +22,15 @@ export default function SquaresDemo() {
           hoverFillColor="#777"
         />
       </div>
-      <div className="absolute inset-0 flex md:items-center justify-center ">
-        <img 
-            src= 'animatedmaci.gif' 
-            alt="Remote GIF"
-            className="h-72 w-72"
+      <BorderTrail
+        style={{
+          boxShadow:
+            "0px 0px 60px 30px rgb(255 255 255 / 50%), 0 0 100px 60px rgb(0 0 0 / 50%), 0 0 140px 90px rgb(0 0 0 / 50%)",
+        }}
+        size={100}
         />
-      </div>
     </div>
+    </>
+  
   )
 }
